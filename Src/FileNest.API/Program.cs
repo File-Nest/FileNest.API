0d3579382b1f73dfbf44cb8b9fe31cc17f1fb8d2
Kedarieshwar <kedarieshwarkrishna@gmail.com>
2026-08-25 14:41:23 +0530

    Enabled Swagger for Staging and Production environments

--- Src/FileNest.API/Program.cs
+++ Src/FileNest.API/Program.cs
@@ -17,7 +17,7 @@ namespace FileNest.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+            if (app.Environment.IsProduction() || app.Environment.IsStaging())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
