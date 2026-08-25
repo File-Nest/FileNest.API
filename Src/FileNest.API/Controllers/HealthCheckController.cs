b762af48a0b11339760e7655bbfdb79980d58138
Kedarieshwar <kedarieshwarkrishna@gmail.com>
2026-08-25 14:20:21 +0530

    Added HealthCheck, Updated Dockerfile & Removed WeatherForecast

---
+++ Src/FileNest.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileNest.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : Controller
+    { 
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "API Working..."
+            });
+        }
+    }
+}
--- Src/FileNest.API/Program.cs
+++ Src/FileNest.API/Program.cs
@@ -17,7 +17,7 @@ namespace FileNest.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
